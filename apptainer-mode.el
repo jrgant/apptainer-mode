f53b0a31e10a611e6eedc226330b36523c14746f
Jason Gantenberg <jason.gantenberg@gmail.com>
2024-05-09 12:39:00 -0400

    initial file setup

---
+++ apptainer-mode.el
@@ -0,0 +1,30 @@
+;;; apptainer-mode.el --- Major mode for editing Apptainer definition files -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2024 Jason Gantenberg
+;; Author: Jason Gantenberg <jason.gantenberg@gmail.com>
+;; Package-Requires: ((emacs "29.3"))
+;; Homepage: https://github.com/jrgant/apptainer-mode
+;; Version: 0.1
+;; Keywords: apptainer languages processes tools
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; APPTAINER-MODE is licensed under the GNU General Public License version 3,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; See <https://www.gnu.org/licenses/> and or the package repository's
+;; <https://github.com/jrgant/apptainer-mode/blob/main/LICENSE> for more information.
+
+;;; Commentary:
+
+;; This package provides a major mode for editing Apptainer definition files.
+
+;;; Code:
+
+(provide 'apptainer-mode)
+
+;;; apptainer-mode.el ends here
